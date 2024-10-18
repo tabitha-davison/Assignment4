490ce4ad31b32708c399aa53ecfc85ce3fda0022
T17084 <tdavison@olin.edu>
2024-10-18 11:22:02 -0400

    Create explicit_RK_step.m
    Just the function from assignment

---
+++ explicit_RK_step.m
@@ -0,0 +1,23 @@
+
+
+%This function computes the value of X at the next time step
+%for any arbitrary RK method
+%INPUTS:
+%rate_func_in: the function used to compute dXdt. rate_func_in will
+% have the form: dXdt = rate_func_in(t,X) (t is before X)
+%t: the value of time at the current step
+%XA: the value of X(t)
+%h: the time increment for a single step i.e. delta_t = t_{n+1} - t_{n}
+%BT_struct: a struct that contains the Butcher tableau
+% BT_struct.A: matrix of a_{ij} values
+% BT_struct.B: vector of b_i values
+% BT_struct.C: vector of c_i values
+%OUTPUTS:
+%XB: the approximate value for X(t+h) (the next step)
+% formula depends on the integration method used
+%num_evals: A count of the number of times that you called
+% rate_func_in when computing the next step
+
+function [XB, num_evals] = explicit_RK_step(rate_func_in,t,XA,h,BT_struct)
+%your code here
+end
